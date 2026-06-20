0ae677ee5472c943f89c9d24eb64139e1f18f524
Faizul <faizulrahmats788@gmail.com>
2026-06-20 17:18:23 +0700

    feat: add Putusan private fields

--- src/model/Putusan.java
+++ src/model/Putusan.java
@@ -10,4 +10,21 @@ public class Putusan extends DokumenHukum implements Laporan {
     public String getKategoriHukuman() {
         return "";
     }
+
+    private String pengadilan;
+    private String tanggalPutusan;
+    private String namaTerdakwa;
+    private int umurTerdakwa;
+    private String jenisNarkotika;
+    private double beratBarangBukti;
+    private String pasalDilanggar;
+    private String peranTerdakwa;
+    private int vonisHukuman;
+    private double vonisDenda;
+    private String namaHakim;
+
+    private static int jumlahDibuat = 0;
+
+    
 }
+
