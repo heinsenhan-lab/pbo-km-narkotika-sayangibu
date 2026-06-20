bea7748ad45dedb5e116e0bc8da91aecaac3786b
Iman <muhammadnuruliman@webmail.umm.ac.id>
2026-06-20 17:31:02 +0700

    feat: add case indentity input from

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -1,31 +1,39 @@
 package view;
 
-public class ConsoleView {
-}
-package view;
-
-public class ConsoleView {
-
-}
-
-package view;
-
 import java.util.Scanner;
 
 public class ConsoleView {
     private Scanner sc = new Scanner(System.in);
-}
 
-public int tampilkanMenu() {
-    System.out.println("\n===== KMS PUTUSAN NARKOTIKA =====");
-    System.out.println("1. Tambah Putusan");
-    System.out.println("2. Tampilkan Semua Putusan");
-    System.out.println("3. Cari Putusan by Nomor");
-    System.out.println("4. Cari Putusan by Nama");
-    System.out.println("5. Statistik");
-    System.out.println("6. Hapus Putusan");
-    System.out.println("7. Keluar");
-    System.out.print("Pilih menu: ");
+    public int tampilkanMenu() {
+        System.out.println("\n===== KMS PUTUSAN NARKOTIKA =====");
+        System.out.println("1. Tambah Putusan");
+        System.out.println("2. Tampilkan Semua Putusan");
+        System.out.println("3. Cari Putusan by Nomor");
+        System.out.println("4. Cari Putusan by Nama");
+        System.out.println("5. Statistik");
+        System.out.println("6. Hapus Putusan");
+        System.out.println("7. Keluar");
+        System.out.print("Pilih menu: ");
 
-    return Integer.parseInt(sc.nextLine());
+        return Integer.parseInt(sc.nextLine());
+    }
+
+    public String[] inputFormPutusan() {
+        String[] data = new String[12];
+
+        System.out.print("Nomor Perkara: ");
+        data[0] = sc.nextLine();
+
+        System.out.print("Pengadilan: ");
+        data[1] = sc.nextLine();
+
+        System.out.print("Tanggal Putusan: ");
+        data[2] = sc.nextLine();
+
+        System.out.print("Nama Terdakwa: ");
+        data[3] = sc.nextLine();
+
+        return data;
+    }
 }
