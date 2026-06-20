7ea2abbcc6c65af6a8bf8b4cbcb5ee68f94b9e43
Iman <muhammadnuruliman@webmail.umm.ac.id>
2026-06-20 17:26:25 +0700

    fear: add console main menu

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -15,3 +15,17 @@ import java.util.Scanner;
 public class ConsoleView {
     private Scanner sc = new Scanner(System.in);
 }
+
+public int tampilkanMenu() {
+    System.out.println("\n===== KMS PUTUSAN NARKOTIKA =====");
+    System.out.println("1. Tambah Putusan");
+    System.out.println("2. Tampilkan Semua Putusan");
+    System.out.println("3. Cari Putusan by Nomor");
+    System.out.println("4. Cari Putusan by Nama");
+    System.out.println("5. Statistik");
+    System.out.println("6. Hapus Putusan");
+    System.out.println("7. Keluar");
+    System.out.print("Pilih menu: ");
+
+    return Integer.parseInt(sc.nextLine());
+}
