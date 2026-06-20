a8f80bfd59ded1cd2fac1edfbc1e84f9338810a6
Farhan <heinsen.han@gmail.com>
2026-06-20 17:19:56 +0700

    feat: add display and search controller methods

--- src/controller/KnowledgeController.java
+++ src/controller/KnowledgeController.java
@@ -1,11 +1,26 @@
 package controller;
 
 import model.KnowledgeRepository;
+import model.Putusan;
+
+import java.util.ArrayList;
 
 public class KnowledgeController {
     private KnowledgeRepository repository;
 
     public KnowledgeController(KnowledgeRepository repository) {
         this.repository = repository;
+    }
+
+    public ArrayList<Putusan> tampilkanSemua() {
+        return repository.getDaftarSemua();
+    }
+
+    public Putusan cariByNomor(String nomor) {
+        return repository.cariByNomor(nomor);
+    }
+
+    public ArrayList<Putusan> cariByNama(String nama) {
+        return repository.cariByNama(nama);
     }
 }
