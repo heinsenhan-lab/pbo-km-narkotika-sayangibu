6a9a14e0cec4f8f544ecaf8dde4c85872ed76ead
Faizul <faizulrahmats788@gmail.com>
2026-06-20 17:23:05 +0700

    feat: add Putusan getters and setters

--- src/model/Putusan.java
+++ src/model/Putusan.java
@@ -47,5 +47,33 @@ public class Putusan extends DokumenHukum implements Laporan {
         this.namaHakim = namaHakim;
         jumlahDibuat++;
     }
+
+    public static int getJumlahDibuat() {
+        return jumlahDibuat;
+    }
+
+    public String getPengadilan() { return pengadilan; }
+    public String getTanggalPutusan() { return tanggalPutusan; }
+    public String getNamaTerdakwa() { return namaTerdakwa; }
+    public int getUmurTerdakwa() { return umurTerdakwa; }
+    public String getJenisNarkotika() { return jenisNarkotika; }
+    public double getBeratBarangBukti() { return beratBarangBukti; }
+    public String getPasalDilanggar() { return pasalDilanggar; }
+    public String getPeranTerdakwa() { return peranTerdakwa; }
+    public int getVonisHukuman() { return vonisHukuman; }
+    public double getVonisDenda() { return vonisDenda; }
+    public String getNamaHakim() { return namaHakim; }
+
+    public void setPengadilan(String pengadilan) { this.pengadilan = pengadilan; }
+    public void setTanggalPutusan(String tanggalPutusan) { this.tanggalPutusan = tanggalPutusan; }
+    public void setNamaTerdakwa(String namaTerdakwa) { this.namaTerdakwa = namaTerdakwa; }
+    public void setUmurTerdakwa(int umurTerdakwa) { this.umurTerdakwa = umurTerdakwa; }
+    public void setJenisNarkotika(String jenisNarkotika) { this.jenisNarkotika = jenisNarkotika; }
+    public void setBeratBarangBukti(double beratBarangBukti) { this.beratBarangBukti = beratBarangBukti; }
+    public void setPasalDilanggar(String pasalDilanggar) { this.pasalDilanggar = pasalDilanggar; }
+    public void setPeranTerdakwa(String peranTerdakwa) { this.peranTerdakwa = peranTerdakwa; }
+    public void setVonisHukuman(int vonisHukuman) { this.vonisHukuman = vonisHukuman; }
+    public void setVonisDenda(double vonisDenda) { this.vonisDenda = vonisDenda; }
+    public void setNamaHakim(String namaHakim) { this.namaHakim = namaHakim; }
 }
 
