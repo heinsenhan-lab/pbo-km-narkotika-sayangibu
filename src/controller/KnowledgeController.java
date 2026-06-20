a42e1f6165438d7924a97fb88baafb773dde8b96
Farhan <heinsen.han@gmail.com>
2026-06-20 17:18:14 +0700

    feat: add controller repository constructor

--- src/controller/KnowledgeController.java
+++ src/controller/KnowledgeController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import model.KnowledgeRepository;
+
 public class KnowledgeController {
+    private KnowledgeRepository repository;
+
+    public KnowledgeController(KnowledgeRepository repository) {
+        this.repository = repository;
+    }
 }
