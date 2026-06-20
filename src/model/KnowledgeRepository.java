dea80c720f22e659b65daeac5c4fad765e6be4a3
Faizul <faizulrahmats788@gmail.com>
2026-06-20 17:42:27 +0700

    feat: add repository search and delete

--- src/model/KnowledgeRepository.java
+++ src/model/KnowledgeRepository.java
@@ -3,6 +3,7 @@ package model;
 import java.util.ArrayList;
 
 public class KnowledgeRepository {
+
     private ArrayList<Putusan> daftarPutusan = new ArrayList<>();
 
     public void simpan(Putusan p) {
@@ -15,5 +16,43 @@ public class KnowledgeRepository {
 
     public int getTotalData() {
         return daftarPutusan.size();
+    }
+
+    public Putusan cariByNomor(String nomor) {
+        for (Putusan p : daftarPutusan) {
+            if (p.getNomorPerkara().equalsIgnoreCase(nomor)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Putusan> cariByNama(String nama) {
+
+        ArrayList<Putusan> hasil = new ArrayList<>();
+
+        for (Putusan p : daftarPutusan) {
+
+            if (p.getNamaTerdakwa()
+                    .toLowerCase()
+                    .contains(nama.toLowerCase())) {
+
+                hasil.add(p);
+            }
+        }
+
+        return hasil;
+    }
+
+    public boolean hapus(String nomor) {
+
+        Putusan p = cariByNomor(nomor);
+
+        if (p != null) {
+            daftarPutusan.remove(p);
+            return true;
+        }
+
+        return false;
     }
 }
