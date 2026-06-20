50c7057bbe1f8659fd540c4faf4ad951765609af
Farhan <heinsen.han@gmail.com>
2026-06-20 17:15:19 +0700

    feat: add number parsing utilities

--- src/util/InputHandler.java
+++ src/util/InputHandler.java
@@ -3,5 +3,13 @@ package util;
 public class InputHandler {
     public static boolean validasiString(String value) {
         return value != null && !value.trim().isEmpty();
+
+        public static int parseInt(String value) throws NumberFormatException {
+            return Integer.parseInt(value);
+        }
+
+        public static double parseDouble(String value) throws NumberFormatException {
+            return Double.parseDouble(value);
+        }
     }
 }
