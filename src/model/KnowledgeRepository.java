1850b218db3df1c9cac28eb7dfddb2a703b466e4
Faizul <faizulrahmats788@gmail.com>
2026-06-20 17:39:04 +0700

    feat: add repository data storage

--- src/model/KnowledgeRepository.java
+++ src/model/KnowledgeRepository.java
@@ -1,4 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+
 public class KnowledgeRepository {
+    private ArrayList<Putusan> daftarPutusan = new ArrayList<>();
+
+    public void simpan(Putusan p) {
+        daftarPutusan.add(p);
+    }
+
+    public ArrayList<Putusan> getDaftarSemua() {
+        return daftarPutusan;
+    }
+
+    public int getTotalData() {
+        return daftarPutusan.size();
+    }
 }
